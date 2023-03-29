4ab0729029d64afb653db6dcf173f531d193ac5b
hellgateway <yakobusfrans@gmail.com>
2023-03-29 11:31:27 +0700

    add bg-white

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -8,24 +8,25 @@
         <template #default>
           <div>
             <div class="event-thumbnail bg-cover bg-center " style="padding-bottom: 141%;" :style="backgroundImage" />
-          
-            <div class="event-detail pt-2 pb-4 px-4">
-              <span class="p-1 px-2 text-sm rounded-xl bg-gray-400 text-white"> {{ event.category  }}</span>
-              <h1 class="text-[28px] font-bold pt-4">{{ event.title  }}</h1>
-              <p class="pt-1 text-sm">{{ event.location }}</p>
-              <div class="pt-1 text-sm">
-                {{ event.dateStart }}
-                <template v-if="event.dateEnd">
-                  - {{ event.dateEnd }}
-                </template>
+            <div class="bg-white ">
+              <div class="event-detail pt-2 pb-4 px-4">
+                <span class="p-1 px-2 text-sm rounded-xl bg-gray-400 text-white"> {{ event.category  }}</span>
+                <h1 class="text-[28px] font-bold pt-4">{{ event.title  }}</h1>
+                <p class="pt-1 text-sm">{{ event.location }}</p>
+                <div class="pt-1 text-sm">
+                  {{ event.dateStart }}
+                  <template v-if="event.dateEnd">
+                    - {{ event.dateEnd }}
+                  </template>
+                </div>
+              </div>
+            
+              <div class="event-description px-4 text-sm pb-4">
+                <p>{{ event.description }}</p>
               </div>
             </div>
           
-            <div class="event-description px-4 text-sm pb-4">
-              <p>{{ event.description }}</p>
-            </div>
-          
-            <div class="event-content px-4">
+            <div class="event-content px-4 pt-4">
               <event-accordion title="registrasi">
                 <template #content>
                   <form-register />
