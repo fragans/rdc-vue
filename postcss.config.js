864005d4d3decd0c269f7b39f62c1960ade5d835
hellgateway <yakobusfrans@gmail.com>
2023-04-06 17:29:08 +0700

    fix module not define

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export const plugins = {
-tailwindcss: {},
-autoprefixer: {},
-};
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
