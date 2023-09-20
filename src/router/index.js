283271900587c476b92af3ec834d77bebf9ee136
hellgateway <yakobusfrans@gmail.com>
2023-09-20 15:19:50 +0700

    remove saved post router

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,15 @@ import PagesAllEvent from '../components/pages/allEvent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    if (to.hash) {
-      return { el: to.hash };
-    }
-    return { x: 0, y: 0 };
-  },
+  // scrollBehavior(to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition;
+  //   }
+  //   if (to.hash) {
+  //     return { el: to.hash };
+  //   }
+  //   return { x: 0, y: 0 };
+  // },
   routes: [
     {
       path: '/',
